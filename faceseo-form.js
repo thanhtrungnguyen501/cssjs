19b8e7806e38f0332ffd15942e8b8d5f6b2ed7ba
thanhtrungnguyen501 <58414969+thanhtrungnguyen501@users.noreply.github.com>
2019-12-02 15:18:06 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -111,7 +111,7 @@ jQuery(divpopup).attr('id', 'myModalthanh');
 jQuery(divpopup).attr('class', 'thanhfaceseo-modal');
 jQuery( "body" ).append(divpopup);
 var titleform='Đăng ký Form';
-var urlform="https://docs.google.com/forms/d/e/1FAIpQLSfLmBC0f3ipvca5eZeH48HotuzHmbF96XAMhjaMhOFKjmerRg/formResponse";
+var urlform="https://docs.google.com/forms/u/0/d/e/1FAIpQLSeGBUxGn5IEqaeu9i6h1eUyT1PRvJwxDSppuoukTYbE_xdyPA/formResponse";
 var urlchuyen=window.location.href;
 var hovaten="1225773305";
 var dienthoai="1423166125";
